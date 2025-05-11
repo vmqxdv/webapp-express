6a7ed376d2b27efa3a82ca4142eee01f9c1271df
vmqxdv <vquintilio@gmail.com>
2025-05-11 22:34:26 +0200

    fix: Impostato il percorso della route per i film e aggiornato il nome della variabile di porta

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ require('dotenv').config();
 
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
-const router = require('./routes/posts');
+const PORT = process.env.SITE_PORT || 3000;
+const router = require('./routes/movies');
 
 app.use(express.static('public'));
 
-app.use('/posts', router);
+app.use('/movies', router);
 
 app.listen(PORT, () => {
   console.log(`Server avviato su http://localhost:${PORT}`);
