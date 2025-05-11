2c7fa6bffe9da9983378636aeb70c8a5ba85c400
vmqxdv <vquintilio@gmail.com>
2025-05-11 22:10:35 +0200

    refactor: Utilizzate variabili di ambiente per connessione al db

--- database/mysql.js
+++ database/mysql.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'admin',
-    database: 'webapp-express'
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || '',
+    database: process.env.DB_NAME,
 });
 
 connection.connect((err) => {
