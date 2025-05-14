a0601fdf207bd8d33ef9caa3a90c20ddc863d4e5
vmqxdv <vquintilio@gmail.com>
2025-05-14 17:19:31 +0200

    feat: Aggiunta rotta "reviews"

--- routes/movies.js
+++ routes/movies.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { getAllMovies: index, getMovieById: show } = require('../controllers/moviesController');
+const { getAllMovies: index, getMovieById: show, addReview: post } = require('../controllers/moviesController');
 
 router.get('/', index);
 router.get('/:id', show);
+router.post('/:id/reviews', post);
 
 module.exports = router;
